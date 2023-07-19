e5354fea0ad37c5db32b5a3095dae47c5e8879b5
rachelswart <rachel.swart@cabinetoffice.gov.uk>
2023-07-20 00:03:13 +0100

    test

--- src/main/java/gov/cabinetoffice/gap/adminbackend/services/SubmissionsService.java
+++ src/main/java/gov/cabinetoffice/gap/adminbackend/services/SubmissionsService.java
@@ -50,6 +50,7 @@ import org.springframework.transaction.annotation.Transactional;
 @Slf4j
 public class SubmissionsService {
 
+    //TODO testing github, remove this comment
     static final List<String> SPOTLIGHT_HEADERS = Arrays.asList("Application number (required)",
             "Organisation name (required)", "Address street (optional)", "Address town (optional)",
             "Address county (optional)", "Address postcode (required)", "Application amount (required)",
