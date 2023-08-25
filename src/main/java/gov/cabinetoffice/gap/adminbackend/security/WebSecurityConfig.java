30ad9385fdd3f6aa1fb54647c4cd1c9af313021c
john-tco <john.morley@cabinetoffice.gov.uk>
2023-08-25 18:29:11 +0100

    call filter

--- src/main/java/gov/cabinetoffice/gap/adminbackend/security/WebSecurityConfig.java
+++ src/main/java/gov/cabinetoffice/gap/adminbackend/security/WebSecurityConfig.java
@@ -1,14 +1,21 @@
 package gov.cabinetoffice.gap.adminbackend.security;
 
+import gov.cabinetoffice.gap.adminbackend.services.JwtService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.access.intercept.AuthorizationFilter;
+import org.springframework.security.web.authentication.AuthenticationFilter;
 import org.springframework.security.web.authentication.HttpStatusEntryPoint;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
@@ -40,6 +47,9 @@ public class WebSecurityConfig {
 
                 .formLogin().disable().httpBasic().disable().logout().disable().csrf().disable().exceptionHandling()
                 .authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED));
+
+         http.addFilterAfter(jwtTokenFilter, UsernamePasswordAuthenticationFilter.class);
+
         return http.build();
     }
 
