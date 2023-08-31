9ee89fdbb294762c56327f96594838f96c98f96b
dominicwest <dominic.west@cabinetoffice.gov.uk>
2023-08-31 11:31:07 +0100

    Adding unique constraint on grant_applicant_organisation_profile table

--- src/main/resources/db/migration/V1_64__add_unique_constraints_to_users.sql
+++ src/main/resources/db/migration/V1_64__add_unique_constraints_to_users.sql
@@ -1,3 +1,4 @@
 ALTER TABLE gap_user ADD CONSTRAINT unique_user_sub UNIQUE (user_sub);
 ALTER TABLE grant_applicant ADD CONSTRAINT unique_user_id UNIQUE (user_id);
 ALTER TABLE grant_admin ADD CONSTRAINT unique_user_id UNIQUE (user_id);
+ALTER TABLE grant_applicant_organisation_profile ADD CONSTRAINT unique_applicant_id UNIQUE (applicant_id);
