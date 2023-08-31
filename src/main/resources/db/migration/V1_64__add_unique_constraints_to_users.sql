0afbefb7f79c160395321f6c18fdcc08bf4460cd
dominicwest <dominic.west@cabinetoffice.gov.uk>
2023-08-31 11:21:06 +0100

    Adding constraint on gap_admin table

--- src/main/resources/db/migration/V1_64__add_unique_constraints_to_users.sql
+++ src/main/resources/db/migration/V1_64__add_unique_constraints_to_users.sql
@@ -1,2 +1,3 @@
 ALTER TABLE gap_user ADD CONSTRAINT unique_user_sub UNIQUE (user_sub);
 ALTER TABLE grant_applicant ADD CONSTRAINT unique_user_id UNIQUE (user_id);
+ALTER TABLE grant_admin ADD CONSTRAINT unique_user_id UNIQUE (user_id);
