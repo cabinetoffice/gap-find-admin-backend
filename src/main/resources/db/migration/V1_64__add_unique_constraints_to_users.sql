5d6832a8cfd3ff754877bd9396a90b2fb8a7a54c
dominicwest <dominic.west@cabinetoffice.gov.uk>
2023-08-31 15:33:17 +0100

    Removing unique constraints on tables with foreign key constraints

--- src/main/resources/db/migration/V1_64__add_unique_constraints_to_users.sql
+++ src/main/resources/db/migration/V1_64__add_unique_constraints_to_users.sql
@@ -1,4 +1,2 @@
 ALTER TABLE gap_user ADD CONSTRAINT unique_user_sub UNIQUE (user_sub);
 ALTER TABLE grant_applicant ADD CONSTRAINT unique_user_id UNIQUE (user_id);
-ALTER TABLE grant_admin ADD CONSTRAINT unique_user_id UNIQUE (user_id);
-ALTER TABLE grant_applicant_organisation_profile ADD CONSTRAINT unique_applicant_id UNIQUE (applicant_id);
