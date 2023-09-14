74f8bbb598ae8b07eb588c2670e57d21f22885e8
conor <conor.fayle@and.digital>
2023-09-14 16:04:46 +0100

    GAP-2148 | modify scheduled job to pick up adverts that were supposed to be published/unpublished but weren't

---
+++ src/main/resources/db/migration/V1_66__alter_scheduler_database_view.sql
@@ -0,0 +1,22 @@
+-- Replace view for scheduler
+
+DROP VIEW IF EXISTS ADVERT_SCHEDULER_VIEW;
+
+CREATE VIEW ADVERT_SCHEDULER_VIEW AS
+SELECT GRANT_ADVERT_ID AS ID,
+    CASE
+        WHEN
+            STATUS = 'SCHEDULED'
+            AND (OPENING_DATE AT TIME ZONE 'Europe/London')::date <= (NOW() AT TIME ZONE 'Europe/London')::date
+        THEN 'PUBLISH'
+        WHEN
+            STATUS = 'PUBLISHED'
+            AND (CLOSING_DATE AT TIME ZONE 'Europe/London')::date <= ((NOW() AT TIME ZONE 'Europe/London')::date - interval '1' DAY)
+        THEN 'UNPUBLISH'
+    END AS ACTION
+FROM GRANT_ADVERT
+WHERE (
+    STATUS = 'SCHEDULED' AND (OPENING_DATE AT TIME ZONE 'Europe/London')::date <= (NOW() AT TIME ZONE 'Europe/London')::date
+) OR (
+    STATUS = 'PUBLISHED' AND (CLOSING_DATE AT TIME ZONE 'Europe/London')::date <= ((NOW() AT TIME ZONE 'Europe/London')::date - interval '1' DAY)
+);
