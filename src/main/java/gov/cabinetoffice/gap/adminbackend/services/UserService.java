3b2a37cf6680dad8238e48f73c51a310c6ee95ce
john-tco <john.morley@cabinetoffice.gov.uk>
2023-09-14 09:59:15 +0100

    linter

--- src/main/java/gov/cabinetoffice/gap/adminbackend/services/UserService.java
+++ src/main/java/gov/cabinetoffice/gap/adminbackend/services/UserService.java
@@ -43,7 +43,8 @@ public class UserService {
     }
 
     public Boolean verifyAdminRoles(final String emailAddress, final String roles) {
-        // TODO: after admin-session token handling is aligned with applicant we should use '/is-user-logged-in'
+        // TODO: after admin-session token handling is aligned with applicant we should
+        // use '/is-user-logged-in'
         final String url = userServiceConfig.getDomain() + "/v2/validateSessionsRoles?emailAddress=" + emailAddress
                 + "&roles=" + roles;
         final HttpEntity<String> requestEntity = new HttpEntity<>(null);
