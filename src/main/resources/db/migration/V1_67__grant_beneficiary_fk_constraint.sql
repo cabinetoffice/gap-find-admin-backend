0a6ba9b7d3f47b4897395993626880c2401e52e1
dominicwest <dominic.west@cabinetoffice.gov.uk>
2023-09-18 16:12:51 +0100

    GAP-2070 fixing not null constraint on created_by

--- src/main/resources/db/migration/V1_67__grant_beneficiary_fk_constraint.sql
+++ src/main/resources/db/migration/V1_67__grant_beneficiary_fk_constraint.sql
@@ -1,4 +1,6 @@
 alter table public.grant_beneficiary
+alter column created_by drop not null,
+drop constraint if exists created_by_fkey,
 add constraint created_by_fkey
     foreign key (created_by)
     references public.grant_applicant (id) match simple
