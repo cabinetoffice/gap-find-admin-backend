c1de57ca3baa6d19bb1c4649943f80aaef112457
dominicwest <dominic.west@cabinetoffice.gov.uk>
2023-11-06 11:24:39 +0000

    GAP-1784: Adding get an admins schemes endpoint. Restricting to SAs only

--- src/main/java/gov/cabinetoffice/gap/adminbackend/repositories/SchemeRepository.java
+++ src/main/java/gov/cabinetoffice/gap/adminbackend/repositories/SchemeRepository.java
@@ -1,12 +1,13 @@
 package gov.cabinetoffice.gap.adminbackend.repositories;
 
-import java.util.List;
-
 import gov.cabinetoffice.gap.adminbackend.entities.SchemeEntity;
-
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface SchemeRepository extends JpaRepository<SchemeEntity, Integer> {
@@ -15,4 +16,7 @@ public interface SchemeRepository extends JpaRepository<SchemeEntity, Integer> {
 
     List<SchemeEntity> findByCreatedByOrderByCreatedDateDesc(Integer grantAdminId, Pageable pageable);
 
+    @Query("select s from SchemeEntity s where s.createdBy = ?1")
+    @PreAuthorize("hasRole('SUPER_ADMIN')")
+    List<SchemeEntity> findByCreatedBy(Integer createdBy);
 }
