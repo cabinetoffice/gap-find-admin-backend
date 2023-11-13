1a30d76260deb2dd12d7c0d8fbf1b7b399db01df
Gavin Cook <gavin.cook1@cabinetoffice.gov.uk>
2023-11-13 14:50:31 +0000

    updating migration to check if columns exist

--- src/main/resources/db/migration/V1_75__drop_created_by_and_updated_by_on_spotlight_tables.sql
+++ src/main/resources/db/migration/V1_75__drop_created_by_and_updated_by_on_spotlight_tables.sql
@@ -1,14 +1,14 @@
 -- drop the created by and last updated by columns from spotlight submission table
 ALTER TABLE spotlight_submission
-DROP COLUMN created_by; 
+DROP COLUMN IF EXISTS created_by;
 
 ALTER TABLE spotlight_submission
-DROP COLUMN last_updated_by;
+DROP COLUMN IF EXISTS last_updated_by;
 
 
 -- drop the created by and last updated by columns from spotlight batch table
 ALTER TABLE spotlight_batch
-DROP COLUMN created_by; 
+DROP COLUMN IF EXISTS  created_by;
 
 ALTER TABLE spotlight_batch
-DROP COLUMN last_updated_by; 
+DROP COLUMN IF EXISTS  last_updated_by;
