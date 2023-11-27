0fe995ececbfec9d7a13145a3e26b090e51d00ee
dominicwest <dominic.west@cabinetoffice.gov.uk>
2023-11-27 11:37:12 +0000

    Fixing jackson object mapper

--- src/main/java/gov/cabinetoffice/gap/adminbackend/config/JacksonConfig.java
+++ src/main/java/gov/cabinetoffice/gap/adminbackend/config/JacksonConfig.java
@@ -1,15 +1,25 @@
 package gov.cabinetoffice.gap.adminbackend.config;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import static com.fasterxml.jackson.databind.DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES;
+import static com.fasterxml.jackson.databind.MapperFeature.DEFAULT_VIEW_INCLUSION;
+import static com.fasterxml.jackson.databind.SerializationFeature.WRITE_DATES_AS_TIMESTAMPS;
 
 @Configuration
 public class JacksonConfig {
 
     @Bean
     public ObjectMapper getObjectMapper() {
-        return new ObjectMapper();
+        final ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.disable(WRITE_DATES_AS_TIMESTAMPS);
+        objectMapper.disable(DEFAULT_VIEW_INCLUSION);
+        objectMapper.disable(FAIL_ON_UNKNOWN_PROPERTIES);
+        objectMapper.registerModule(new JavaTimeModule());
+        return objectMapper;
     }
 
 }
