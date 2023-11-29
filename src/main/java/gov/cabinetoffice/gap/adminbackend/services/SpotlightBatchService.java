6cfc89815b2aadc21ec1000001c545834eba7a47
rachelswart <99667350+rachelswart@users.noreply.github.com>
2023-11-29 14:20:36 +0000

    TMI2-454: send Spotlight API error support email (#111)
    * send spotlightApiError support email and tests
    
    * Formatting

--- src/main/java/gov/cabinetoffice/gap/adminbackend/services/SpotlightBatchService.java
+++ src/main/java/gov/cabinetoffice/gap/adminbackend/services/SpotlightBatchService.java
@@ -342,7 +342,10 @@ public class SpotlightBatchService {
         }
         catch (HttpServerErrorException e) {
             if (e.getStatusCode().is5xxServerError()) { // 5xx codes
-                // TODO send API error support email here
+                log.info("Sending spotlight API error support email using SNS for status code: " + e.getStatusCode());
+                final String snsResponse = snsService.spotlightApiError();
+                log.info(snsResponse);
+
                 log.error("Hitting {} returned status code {} with body {}", draftAssessmentsEndpoint,
                         e.getStatusCode(), e.getResponseBodyAsString());
             }
