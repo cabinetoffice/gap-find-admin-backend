60effc2fd136a520299eca1d09163c961dc83823
john-tco <john.morley@cabinetoffice.gov.uk>
2024-02-01 11:37:22 +0000

    amend thrown error message

--- src/main/java/gov/cabinetoffice/gap/adminbackend/controllers/ApplicationFormSectionsController.java
+++ src/main/java/gov/cabinetoffice/gap/adminbackend/controllers/ApplicationFormSectionsController.java
@@ -170,7 +170,8 @@ public class ApplicationFormSectionsController {
     public ResponseEntity<Void> updateSectionTitle(final @PathVariable Integer applicationId,
             final @PathVariable String sectionId, final @RequestBody @Validated PostSectionDTO sectionDTO) {
         if (Objects.equals(sectionId, "ELIGIBILITY") || Objects.equals(sectionId, "ESSENTIAL")) {
-            return new ResponseEntity(new GenericErrorDTO("You cannot update the status of a custom section"),
+            return new ResponseEntity(
+                    new GenericErrorDTO("You cannot update the title of a non-custom section"),
                     HttpStatus.BAD_REQUEST);
         }
         this.applicationFormSectionService.updateSectionTitle(applicationId, sectionId, sectionDTO.getSectionTitle());
