f7c66ce890d9009619e8c29a123858ef56b76844
Iain Cooper <iain.cooper@cabinetoffice.gov.uk>
2024-02-13 11:20:42 +0000

    TMI2-630 - Add grant_export_batch table

--- src/main/java/gov/cabinetoffice/gap/adminbackend/enums/GrantExportStatus.java
+++ src/main/java/gov/cabinetoffice/gap/adminbackend/enums/GrantExportStatus.java
@@ -2,6 +2,6 @@ package gov.cabinetoffice.gap.adminbackend.enums;
 
 public enum GrantExportStatus {
 
-    REQUESTED, PROCESSING, COMPLETE, ERROR, EXPIRED, NOT_STARTED
+    REQUESTED, PROCESSING, COMPLETE, ERROR, EXPIRED, NOT_STARTED, NOT_GENERATED
 
 }
