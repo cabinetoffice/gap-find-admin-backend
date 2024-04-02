502b4897ac15c3d1a2c69c3401efdbfdcb95a3e4
dominicwest <dominic.west@cabinetoffice.gov.uk>
2024-04-02 11:31:02 +0100

    GAP-2533: Including release time

--- src/main/resources/db/migration/V1_101__retrospectively_fix_bst_ads.sql
+++ src/main/resources/db/migration/V1_101__retrospectively_fix_bst_ads.sql
@@ -1,11 +1,11 @@
 UPDATE grant_advert
     SET opening_date = opening_date - INTERVAL '1 hour'
 WHERE to_char(opening_date, 'TZ') = 'BST'
-    AND created > '2023-09-28'::DATE
+    AND created > '2023-09-28 12:00:00'::TIMESTAMP
 	AND status = 'SCHEDULED';
 
 UPDATE grant_advert
     SET closing_date = closing_date - INTERVAL '1 hour'
 WHERE to_char(closing_date, 'TZ') = 'BST'
-    AND created > '2023-09-28'::DATE
+    AND created > '2023-09-28 12:00:00'::TIMESTAMP
 	AND status = 'PUBLISHED';
