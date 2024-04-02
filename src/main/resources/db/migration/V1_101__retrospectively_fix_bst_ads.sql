8b87271f4e09cc0e27d8a6a76eced97df7aa6130
dominicwest <dominic.west@cabinetoffice.gov.uk>
2024-04-02 12:11:25 +0100

    GAP-2533: Overriding UTC opening/closing dates as Europe/London time

--- src/main/resources/db/migration/V1_101__retrospectively_fix_bst_ads.sql
+++ src/main/resources/db/migration/V1_101__retrospectively_fix_bst_ads.sql
@@ -1,11 +1,9 @@
 UPDATE grant_advert
-    SET opening_date = opening_date - INTERVAL '1 hour'
-WHERE to_char(opening_date, 'TZ') = 'BST'
-    AND created > '2023-09-28 12:00:00'::TIMESTAMP
+    SET opening_date = opening_date AT TIME ZONE 'Europe/London'
+WHERE created > '2023-09-28 12:00:00'::TIMESTAMP
 	AND status = 'SCHEDULED';
 
 UPDATE grant_advert
-    SET closing_date = closing_date - INTERVAL '1 hour'
-WHERE to_char(closing_date, 'TZ') = 'BST'
-    AND created > '2023-09-28 12:00:00'::TIMESTAMP
+    SET closing_date = closing_date AT TIME ZONE 'Europe/London'
+WHERE created > '2023-09-28 12:00:00'::TIMESTAMP
 	AND status = 'PUBLISHED';
