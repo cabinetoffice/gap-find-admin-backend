0702972d5834d0aaafff09e5e489899f272b640d
paul-lawlor-tco <145372589+paul-lawlor-tco@users.noreply.github.com>
2024-04-17 17:20:19 +0100

    GAP-2523: Super admin only grant ownership (#289)
    * Clean up logic to make call to user service regardless of role
    
    * Make test more generic, removing uneccesary URI concatenation
    
    * optimize imports

--- src/main/java/gov/cabinetoffice/gap/adminbackend/services/UserService.java
+++ src/main/java/gov/cabinetoffice/gap/adminbackend/services/UserService.java
@@ -15,7 +15,6 @@ import gov.cabinetoffice.gap.adminbackend.repositories.GapUserRepository;
 import gov.cabinetoffice.gap.adminbackend.repositories.GrantAdminRepository;
 import gov.cabinetoffice.gap.adminbackend.repositories.GrantApplicantRepository;
 import gov.cabinetoffice.gap.adminbackend.services.encryption.AwsEncryptionServiceImpl;
-import gov.cabinetoffice.gap.adminbackend.utils.HelperUtils;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.core.ParameterizedTypeReference;
@@ -117,10 +116,8 @@ public class UserService {
     @PreAuthorize("hasAnyRole('ADMIN', 'SUPER_ADMIN')")
     public GrantAdmin getGrantAdminIdFromUserServiceEmail(final String email, final String jwt) {
         try {
-            String roles = HelperUtils.getAdminSessionForAuthenticatedUser().getRoles();
-            String uriRole = roles.contains("SUPER_ADMIN") ? "SUPER_ADMIN" : "ADMIN";
             UserV2DTO response = webClientBuilder.build().get()
-                    .uri(userServiceConfig.getDomain() + "/user/email/" + email + "?role=" + uriRole)
+                    .uri(userServiceConfig.getDomain() + "/user/email/" + email)
                     .cookie(userServiceConfig.getCookieName(), jwt).retrieve().bodyToMono(UserV2DTO.class).block();
 
             return grantAdminRepository.findByGapUserUserSub(response.sub()).orElseThrow(() -> new NotFoundException(
