d6fdcfa6462978bf8633589b5dac14f4e3a63f63
Gavin Cook <gavin.cook1@cabinetoffice.gov.uk>
2024-04-22 12:39:48 +0100

    reverting web client config changes

--- src/main/java/gov/cabinetoffice/gap/adminbackend/config/WebClientConfig.java
+++ src/main/java/gov/cabinetoffice/gap/adminbackend/config/WebClientConfig.java
@@ -2,22 +2,13 @@ package gov.cabinetoffice.gap.adminbackend.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.client.reactive.ClientHttpConnector;
-import org.springframework.http.client.reactive.ReactorClientHttpConnector;
 import org.springframework.web.reactive.function.client.WebClient;
-import reactor.netty.http.client.HttpClient;
 
 @Configuration
 public class WebClientConfig {
 
     @Bean
     public WebClient getWebClient() {
-        return WebClient.builder()
-                .clientConnector(connector())
-                .build();
-    }
-
-    private ClientHttpConnector connector() {
-        return new ReactorClientHttpConnector(HttpClient.newConnection());
+        return WebClient.builder().build();
     }
 }
