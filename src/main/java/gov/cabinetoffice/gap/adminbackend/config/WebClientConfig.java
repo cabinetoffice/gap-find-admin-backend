0cd81ba9140039870c19260f769d0d1d02979c86
Gavin Cook <gavin.cook1@cabinetoffice.gov.uk>
2024-04-22 11:06:19 +0100

    trying to force web client to use new connections

--- src/main/java/gov/cabinetoffice/gap/adminbackend/config/WebClientConfig.java
+++ src/main/java/gov/cabinetoffice/gap/adminbackend/config/WebClientConfig.java
@@ -2,13 +2,22 @@ package gov.cabinetoffice.gap.adminbackend.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.reactive.ClientHttpConnector;
+import org.springframework.http.client.reactive.ReactorClientHttpConnector;
 import org.springframework.web.reactive.function.client.WebClient;
+import reactor.netty.http.client.HttpClient;
 
 @Configuration
 public class WebClientConfig {
 
     @Bean
     public WebClient getWebClient() {
-        return WebClient.builder().build();
+        return WebClient.builder()
+                .clientConnector(connector())
+                .build();
+    }
+
+    private ClientHttpConnector connector() {
+        return new ReactorClientHttpConnector(HttpClient.newConnection());
     }
 }
