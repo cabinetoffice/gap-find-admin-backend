e81c9c3992926725f5ffebac6c160c286de9c905
Gavin Cook <gavin.cook1@cabinetoffice.gov.uk>
2024-04-22 14:35:51 +0100

    adding a max idle time less than the load balancer duration.

--- src/main/java/gov/cabinetoffice/gap/adminbackend/config/WebClientConfig.java
+++ src/main/java/gov/cabinetoffice/gap/adminbackend/config/WebClientConfig.java
@@ -2,22 +2,24 @@ package gov.cabinetoffice.gap.adminbackend.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.client.reactive.ClientHttpConnector;
 import org.springframework.http.client.reactive.ReactorClientHttpConnector;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.netty.http.client.HttpClient;
+import reactor.netty.resources.ConnectionProvider;
+
+import java.time.Duration;
 
 @Configuration
 public class WebClientConfig {
 
     @Bean
     public WebClient getWebClient() {
-        return WebClient.builder()
-                .clientConnector(connector())
+        final ConnectionProvider provider = ConnectionProvider.builder("fixed")
+                .maxIdleTime(Duration.ofSeconds(30))
                 .build();
-    }
 
-    private ClientHttpConnector connector() {
-        return new ReactorClientHttpConnector(HttpClient.newConnection());
+        return WebClient.builder()
+                .clientConnector(new ReactorClientHttpConnector(HttpClient.create(provider)))
+                .build();
     }
 }
