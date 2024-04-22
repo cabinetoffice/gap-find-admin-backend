2708ae12c0d6d9e8920e08c0a79ca1a754a397fe
GavCookCO <99668051+GavCookCO@users.noreply.github.com>
2024-04-22 14:39:02 +0100

    re-adding the web client config (#295)
    * re-adding the web client config
    
    * adding a max idle time less than the load balancer duration.

--- src/main/java/gov/cabinetoffice/gap/adminbackend/config/WebClientConfig.java
+++ src/main/java/gov/cabinetoffice/gap/adminbackend/config/WebClientConfig.java
@@ -2,13 +2,24 @@ package gov.cabinetoffice.gap.adminbackend.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.reactive.ReactorClientHttpConnector;
 import org.springframework.web.reactive.function.client.WebClient;
+import reactor.netty.http.client.HttpClient;
+import reactor.netty.resources.ConnectionProvider;
+
+import java.time.Duration;
 
 @Configuration
 public class WebClientConfig {
 
     @Bean
     public WebClient getWebClient() {
-        return WebClient.builder().build();
+        final ConnectionProvider provider = ConnectionProvider.builder("fixed")
+                .maxIdleTime(Duration.ofSeconds(30))
+                .build();
+
+        return WebClient.builder()
+                .clientConnector(new ReactorClientHttpConnector(HttpClient.create(provider)))
+                .build();
     }
 }
