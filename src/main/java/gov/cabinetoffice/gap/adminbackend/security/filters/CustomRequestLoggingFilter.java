56ab46ee454ada9abe0c25c74c6b456691daa2f2
a-lor-cab <107372333+a-lor-cab@users.noreply.github.com>
2024-04-26 12:58:20 +0100

    Gap 2422/remove health check logs (#298)
    * GAP-2422/remove health check logs
    
    * GAP-2422/add property
    
    * GAP-2422/add loggins to debug
    
    * GAP-2422/fix logic

--- src/main/java/gov/cabinetoffice/gap/adminbackend/security/filters/CustomRequestLoggingFilter.java
+++ src/main/java/gov/cabinetoffice/gap/adminbackend/security/filters/CustomRequestLoggingFilter.java
@@ -1,5 +1,6 @@
 package gov.cabinetoffice.gap.adminbackend.security.filters;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.AbstractRequestLoggingFilter;
 
@@ -7,9 +8,8 @@ import javax.servlet.http.HttpServletRequest;
 import java.util.Set;
 
 @Component
+@Slf4j
 public class CustomRequestLoggingFilter extends AbstractRequestLoggingFilter {
-
-    private final Set<String> excludedUrls = Set.of("/health");
 
     public CustomRequestLoggingFilter() {
         this.setIncludeHeaders(true);
@@ -20,9 +20,10 @@ public class CustomRequestLoggingFilter extends AbstractRequestLoggingFilter {
 
     @Override
     protected boolean shouldLog(HttpServletRequest request) {
-        if (excludedUrls.contains(request.getRequestURI())) {
+        if (request.getRequestURI().endsWith("/health")) {
             return false;
         }
+
         return logger.isDebugEnabled();
     }
 
