a4be0bc7696c23efeaacfd8fbda031cabc0df837
rmohammed-goaco <richard.mohammed@goaco.com>
2025-11-19 12:25:29 +0000

    Release/test deployments main (#317)
    * Add health check endpoint and update security configuration to permit access
    
    * Undo extra indentation

--- src/main/java/gov/cabinetoffice/gap/adminbackend/security/WebSecurityConfig.java
+++ src/main/java/gov/cabinetoffice/gap/adminbackend/security/WebSecurityConfig.java
@@ -31,34 +31,45 @@ public class WebSecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
-        //if you add a path which is hit by the lambda, remember to update also the paths in gov/cabinetoffice/gap/adminbackend/config/LambdasInterceptor.java
+        // if you add a path which is hit by the lambda, remember to update also the
+        // paths in gov/cabinetoffice/gap/adminbackend/config/LambdasInterceptor.java
         http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED).and()
                 .authorizeHttpRequests(auth -> auth
                         .mvcMatchers("/login",
                                 "/health",
+                                "/health/check",
                                 "/emails/sendLambdaConfirmationEmail",
                                 "/users/validateAdminSession",
                                 "/submissions/{submissionId:" + UUID_REGEX_STRING
-                                        + "}/export-batch/{batchExportId:" + UUID_REGEX_STRING + "}/submission",
+                                        + "}/export-batch/{batchExportId:"
+                                        + UUID_REGEX_STRING + "}/submission",
                                 "/submissions/*/export-batch/*/status",
-                                "/submissions/{submissionId:" + UUID_REGEX_STRING + "}/export-batch/{batchExportId:"
+                                "/submissions/{submissionId:" + UUID_REGEX_STRING
+                                        + "}/export-batch/{batchExportId:"
                                         + UUID_REGEX_STRING + "}/s3-object-key",
-                                "/grant-export/{exportId:" + UUID_REGEX_STRING + "}/outstandingCount",
-                                "/grant-export/{exportId:" + UUID_REGEX_STRING + "}/failedCount",
-                                "/grant-export/{exportId:" + UUID_REGEX_STRING + "}/remainingCount",
-                                "/grant-export/{exportId:" + UUID_REGEX_STRING + "}/completed",
-                                "/grant-export/{exportId:" + UUID_REGEX_STRING + "}/batch/status",
-                                "/grant-export/{exportId:" + UUID_REGEX_STRING + "}/batch/s3-object-key",
-                                "/grant-advert/lambda/{grantAdvertId:" + UUID_REGEX_STRING + "}/publish",
-                                "/grant-advert/lambda/{grantAdvertId:" + UUID_REGEX_STRING + "}/unpublish",
+                                "/grant-export/{exportId:" + UUID_REGEX_STRING
+                                        + "}/outstandingCount",
+                                "/grant-export/{exportId:" + UUID_REGEX_STRING
+                                        + "}/failedCount",
+                                "/grant-export/{exportId:" + UUID_REGEX_STRING
+                                        + "}/remainingCount",
+                                "/grant-export/{exportId:" + UUID_REGEX_STRING
+                                        + "}/completed",
+                                "/grant-export/{exportId:" + UUID_REGEX_STRING
+                                        + "}/batch/status",
+                                "/grant-export/{exportId:" + UUID_REGEX_STRING
+                                        + "}/batch/s3-object-key",
+                                "/grant-advert/lambda/{grantAdvertId:"
+                                        + UUID_REGEX_STRING + "}/publish",
+                                "/grant-advert/lambda/{grantAdvertId:"
+                                        + UUID_REGEX_STRING + "}/unpublish",
                                 "/users/migrate",
                                 "/users/delete",
                                 "/users/tech-support-user/**",
                                 "/users/admin-user/**",
                                 "/users/funding-organisation",
                                 "/application-forms/lambda/**",
-                                "/feedback/add"
-                        )
+                                "/feedback/add")
                         .permitAll()
                         .antMatchers("/v3/api-docs/**",
                                 "/swagger-ui/**",
@@ -66,7 +77,8 @@ public class WebSecurityConfig {
                                 "/swagger-ui.html",
                                 "/webjars/**")
                         .permitAll()
-                        .antMatchers("/spotlight-submissions/{spotlightSubmissionId:" + UUID_REGEX_STRING + "}")
+                        .antMatchers("/spotlight-submissions/{spotlightSubmissionId:"
+                                + UUID_REGEX_STRING + "}")
                         .permitAll()
                         .antMatchers("/spotlight-batch/status/**",
                                 "/spotlight-batch",
@@ -77,7 +89,8 @@ public class WebSecurityConfig {
                         .anyRequest()
                         .authenticated())
 
-                .formLogin().disable().httpBasic().disable().logout().disable().csrf().disable().exceptionHandling()
+                .formLogin().disable().httpBasic().disable().logout().disable().csrf().disable()
+                .exceptionHandling()
                 .authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED));
 
         http.addFilterAfter(jwtTokenFilter, UsernamePasswordAuthenticationFilter.class);
