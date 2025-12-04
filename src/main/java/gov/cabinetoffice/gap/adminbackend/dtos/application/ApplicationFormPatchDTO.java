e3e411f232ab19f6e1608456fe26815bac75717c
Richard Mohammed <Richard.Mohammed@goaco.com>
2025-12-04 11:11:57 +0000

    Add validation annotation to ApplicationFormPatchDTO to ensure at least one field is not null. Introduce AtLeastOneFieldNotNull annotation and its validator for this purpose.

--- src/main/java/gov/cabinetoffice/gap/adminbackend/dtos/application/ApplicationFormPatchDTO.java
+++ src/main/java/gov/cabinetoffice/gap/adminbackend/dtos/application/ApplicationFormPatchDTO.java
@@ -1,13 +1,17 @@
 package gov.cabinetoffice.gap.adminbackend.dtos.application;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import gov.cabinetoffice.gap.adminbackend.enums.ApplicationStatusEnum;
+import gov.cabinetoffice.gap.adminbackend.validation.AtLeastOneFieldNotNull;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @NoArgsConstructor
 @JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonIgnoreProperties(ignoreUnknown = false)
+@AtLeastOneFieldNotNull
 public class ApplicationFormPatchDTO {
 
     private ApplicationStatusEnum applicationStatus;
